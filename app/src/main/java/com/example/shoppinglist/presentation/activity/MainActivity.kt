b4a2f48671b79fadfb46dcc83813d3a055dad243
RamzanDev <ramzan1995@gmail.com>
2023-11-23 01:11:46 +0600

    added ShopItemFragment.kt

--- app/src/main/java/com/example/shoppinglist/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/activity/MainActivity.kt
@@ -1,26 +1,68 @@
-package com.example.shoppinglist.presentation
+package com.example.shoppinglist.presentation.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
+import com.example.shoppinglist.presentation.adapter.ShopListAdapter
+import com.example.shoppinglist.presentation.fragment.ShopItemFragment
+import com.example.shoppinglist.presentation.viewmodel.MainViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedListener {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var showListAdapter: ShopListAdapter
+    private var shopItemContainer: FragmentContainerView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        shopItemContainer = findViewById(R.id.shop_item_container)
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) { shopList ->
             Log.d("MainActivityTest", shopList.toString())
             showListAdapter.submitList(shopList)
         }
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.addShopItemButton)
+        buttonAddItem.setOnClickListener {
+            if (isOnePaneMode()) {
+                val intent = ShopItemActivity.newIntentAddItem(this)
+                startActivity(intent)
+            } else {
+                launchFragment(ShopItemFragment.newInstanceAddItem(), "add")
+            }
+        }
+    }
+
+    private fun isOnePaneMode(): Boolean {
+        return shopItemContainer == null
+    }
+
+    override fun onEditingFinished() {
+        Toast.makeText(this@MainActivity, "Success", Toast.LENGTH_SHORT).show()
+        supportFragmentManager.popBackStack()
+    }
+
+    private fun launchFragment(fragment: Fragment, name: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.shop_item_container, fragment)
+            .addToBackStack(name)
+            .commit()
+        Log.d("BackStack________", supportFragmentManager.backStackEntryCount.toString())
+
+    }
+
+    override fun onBackPressed() {
+        supportFragmentManager.popBackStack("add", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        Log.d("BackStack________", supportFragmentManager.backStackEntryCount.toString())
     }
 
     private fun setupRecyclerView() {
@@ -66,7 +108,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         showListAdapter.onShopItemClickListener = { shopItem ->
-            Log.d("MainActivity", shopItem.toString())
+            if (isOnePaneMode()) {
+                val intent = ShopItemActivity.newIntentEditItem(this, shopItem.id)
+                startActivity(intent)
+            } else {
+                launchFragment(ShopItemFragment.newInstanceEditItem(shopItem.id), "edit")
+            }
         }
     }
 
--- app/src/main/java/com/example/shoppinglist/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/viewmodel/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.shoppinglist.presentation
+package com.example.shoppinglist.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.shoppinglist.data.ShopListRepositoryImpl
